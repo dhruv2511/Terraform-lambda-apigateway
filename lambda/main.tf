deb613f6f558ffd902943515929fbf26e645ef2c
Thakkar, Dhruvil <Dhruvil.Thakkar@contracted.pmi.com>
2020-03-11 14:45:27 +0000

    Adding a proper handler to the module

--- lambda/main.tf
+++ lambda/main.tf
@@ -1,5 +1,5 @@
 resource "aws_lambda_function" "lambda" {
-  function_name    = "${var.name}_${var.handler}"
+  function_name    = var.name
   description      = var.description
   role             = var.role
   runtime          = var.runtime
