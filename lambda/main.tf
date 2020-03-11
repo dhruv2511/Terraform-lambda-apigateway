ff36496c57335b3179da333ae6ed43f50b90097e
Thakkar, Dhruvil <Dhruvil.Thakkar@contracted.pmi.com>
2020-03-11 14:49:06 +0000

    Adding the unique function name.

--- lambda/main.tf
+++ lambda/main.tf
@@ -1,9 +1,9 @@
 resource "aws_lambda_function" "lambda" {
-  function_name    = var.name
+  function_name    = "${var.name}_${var.handler}"
   description      = var.description
   role             = var.role
   runtime          = var.runtime
-  handler          = "${var.name}.${var.handler}"
+  handler          = var.handler
   filename         = var.filename
   source_code_hash = var.source_code
 }
