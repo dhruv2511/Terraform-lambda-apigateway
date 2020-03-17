3ff7e43c3939e5cad593c1310f42e1b1da8362b0
Thakkar, Dhruvil <Dhruvil.Thakkar@contracted.pmi.com>
2020-03-17 12:40:48 +0000

    Adding Post methods to the API for testing.
    Signed-off-by: Thakkar, Dhruvil <Dhruvil.Thakkar@contracted.pmi.com>

--- main.tf
+++ main.tf
@@ -107,6 +107,21 @@ resource "aws_lambda_function" "status_dynamodb_lambda" {
   }
 }
 
+
+resource "aws_lambda_function" "post_dynamodb_lambda" {
+  function_name    = "post_dynamodb_lambda"
+  handler          = "portal_request.post_handler"
+  role             = aws_iam_role.iam_role_for_lambda.arn
+  runtime          = "python3.7"
+  source_code_hash = data.archive_file.zipit.output_base64sha256
+  filename         = data.archive_file.zipit.output_path
+  environment {
+    variables = {
+      dynamodb_table = var.dynamodb_table
+    }
+  }
+}
+
 ###############################################################################
 # Logging and Monitoring the API
 ###############################################################################
@@ -225,11 +240,22 @@ module "status_dynamoDB" {
   account_id  = data.aws_caller_identity.current.account_id
 }
 
+module "post_dynamoDB" {
+  source      = "./api_method"
+  rest_api_id = aws_api_gateway_rest_api.portal_api.id
+  resource_id = aws_api_gateway_resource.status_dynamodb_api_rest.id
+  method      = "POST"
+  path        = aws_api_gateway_resource.status_dynamodb_api_rest.path
+  lambda      = aws_lambda_function.status_dynamodb_lambda.id
+  region      = var.aws_region
+  account_id  = data.aws_caller_identity.current.account_id
+}
+
 # We can deploy the API now! (i.e. make it publicly available)
 resource "aws_api_gateway_deployment" "hello_api_deployment" {
   rest_api_id = aws_api_gateway_rest_api.portal_api.id
   stage_name  = "production"
-  description = "Deploy methods: ${module.status_tfe.http_method} ${module.status_dynamoDB.http_method}"
+  description = "Deploy methods: ${module.status_tfe.http_method} ${module.status_dynamoDB.http_method} ${module.post_dynamoDB.http_method}"
 }
 
 resource "aws_api_gateway_usage_plan" "hello_api_plan" {
