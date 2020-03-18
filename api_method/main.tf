5fbcb26b9a3c8cabf751f4b2c7c5ef003e9322c2
Thakkar, Dhruvil <Dhruvil.Thakkar@contracted.pmi.com>
2020-03-18 09:18:17 +0000

    Setting all the parameters that needs to be pass through.
    Signed-off-by: Thakkar, Dhruvil <Dhruvil.Thakkar@contracted.pmi.com>

--- api_method/main.tf
+++ api_method/main.tf
@@ -42,27 +42,7 @@ resource "aws_api_gateway_integration" "request_method_integration" {
     "$key" : "$util.escapeJavaScript($stageVariables.get($key))"
         #if($foreach.hasNext),#end
     #end
-    },
-    "context" : {
-        "account-id" : "$context.identity.accountId",
-        "api-id" : "$context.apiId",
-        "api-key" : "$context.identity.apiKey",
-        "authorizer-principal-id" : "$context.authorizer.principalId",
-        "caller" : "$context.identity.caller",
-        "cognito-authentication-provider" : "$context.identity.cognitoAuthenticationProvider",
-        "cognito-authentication-type" : "$context.identity.cognitoAuthenticationType",
-        "cognito-identity-id" : "$context.identity.cognitoIdentityId",
-        "cognito-identity-pool-id" : "$context.identity.cognitoIdentityPoolId",
-        "http-method" : "$context.httpMethod",
-        "stage" : "$context.stage",
-        "source-ip" : "$context.identity.sourceIp",
-        "user" : "$context.identity.user",
-        "user-agent" : "$context.identity.userAgent",
-        "user-arn" : "$context.identity.userArn",
-        "request-id" : "$context.requestId",
-        "resource-id" : "$context.resourceId",
-        "resource-path" : "$context.resourcePath"
-        }
+    }
 }
   EOF
   }
